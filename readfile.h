4c936928b1fb1a22c01e65bea70a1d70331e48de
yvenkanagari <90996543+yvenkanagari@users.noreply.github.com>
2022-04-14 15:20:53 -0500

    Delete readfile.h

--- readfile.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int open_file(char *filename);
-int read_float(float *f);
-int read_int(int *i);
-int read_string(char *addr);
-int close_file(FILE *stream);
